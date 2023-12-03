e34309c39b5ac89392cbd89e758fa71ec6084b42
yang-yungi <joee1212@gmail.com>
2023-12-03 23:34:26 +0900

    자바 기본기

---
+++ src/variable/Var8.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class Var8 {
+
+    byte b = 127; //-128 ~ 127
+    short s = 32767; //-32768 ~ 32767
+    int i = 2147483647; //-2147483648 ~ 2147483647(약 20억)
+    long l = 9223372036854775807L;
+
+    float f = 10.0f;
+    double d = 10.0;
+
+}
---
+++ src/variable/ex/VarEx1Question.java
@@ -0,0 +1,22 @@
+package variable.ex;
+
+public class VarEx1Question {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 3;
+
+        System.out.println(num1 + num2);
+
+        int num3 = 10;
+        int num4 = 20;
+
+        int sum = num3 + num4;
+        System.out.println(sum);
+
+        long l = 100000000000L;
+        System.out.println(l);
+
+        boolean b =  true;
+        System.out.println(b);
+    }
+}
